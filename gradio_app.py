2df9d3d410118b3e9a7a4a297e24110ce9acdfff
KareldeRijk <k.c.j.derijk@gmail.com>
2025-10-28 22:01:41 +0100

    swapped out openai embedding and multiquery models, ready for testing

--- gradio_app.py
+++ gradio_app.py
@@ -26,5 +26,12 @@ with gr.Blocks(theme=gr.themes.Base(), css="body {background-color: #000000; col
 
 # Launch the app
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo = gr.Interface(
+        fn=respond_to_question, inputs=user_input, outputs=output,
+        title="Rizzbot Chatbot")
+    demo.launch(share = True)
+
+    
+
+    
 
